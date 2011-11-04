adc1c952985819e78f6e3c972e2e9e76db9904a5
Guillaume Olivetti <contact@devduweb.com>
2011-11-04 21:13:08 +0100

    Basic plugin files

---
+++ aospeak.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package AO Speak
+ * @version 0.1
+ */
+/*
+Plugin Name: AO Speak
+Plugin URI: http://boltgun.the-kindred.info
+Description: Display the online list from the AO Speak server for Teamspeak.
+Author: Boltgun
+Version: 0.1
+Author URI: http://boltgun.the-kindred.info
+*/
+// Configuration
+
+
+// Register the widget
+
+// Additional functions
